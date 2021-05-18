37850f17fbf6bae5a7002e46c6993efcbee6537d
Collin D. Bevins <cbevins@montana.com>
2021-05-18 15:32:06 -0600

    'get' uses JSONPlaceHolder api

--- src/routes/get.svelte
+++ src/routes/get.svelte
@@ -1,11 +1,17 @@
 <script>
-  console.log('client: BEGIN get.svelte <script>')
-  function getSomething() {
-  console.log('client: BEGIN get.svelte <script> getSomething()')
-    fetch('getapi')
-  console.log('client: END get.svelte <script> getSomething()')
+  export let data = null
+  let buttonText = 'Get Something'
+
+  const getSomething = async () => {
+    buttonText = 'Getting JSONPlaceHolder todos ...'
+    try {
+      const response = await fetch('getapi')
+      data = await response.json()
+      buttonText = 'Get Something Else'
+    } catch (error) {
+      console.log('client: ERROR get.svelte: ', error)
+    }
   }
-  console.log('client: END get.svelte <script>')
 </script>
 
 <svelte:head>
@@ -14,4 +20,8 @@
 
 <h1>Get something here</h1>
 
-<button on:click={getSomething}>Get!</button>
+<button on:click={getSomething}>{buttonText}</button>
+
+{#if data !== null}
+  <p>userId {data.userId}, id: {data.id}, title: {data.title}, completed: {data.completed}</p>
+{/if}
