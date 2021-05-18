ff1d3653e403290a9080f0d24bc91153dc83f3bf
Collin D. Bevins <cbevins@montana.com>
2021-05-18 12:35:20 -0600

    Hide key

--- src/routes/weatherapi.js
+++ src/routes/weatherapi.js
@@ -1,17 +1,16 @@
 import fetch from 'node-fetch'
-import {weatherApi} from './_models/apis.js'
+import { key } from './_models/weatherApiKey.js'
 
 export async function post (req, res, next) {
   try {
     console.log('server: BEGIN post()')
-    const url = weatherApi.url // 'http://api.weatherapi.com/v1/forecast.json'
-    const key = weatherApi.key // 'key=43956b1f6760417db1d182743212704'
+    const url = 'http://api.weatherapi.com/v1/forecast.json'
     const {lat, lon, days} = req.body
     // 'q' can be a US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name.
     const q = `${lat},${lon}` // '46.85714,-114.0073'
     const aqi = 'no' // get air quality?
     const alerts = 'no' // get weather alerts?
-    const query = `${url}?${key}&days=${days}&q=${q}&aqi=${aqi}&alerts=${alerts}`
+    const query = `${url}?key=${key}&days=${days}&q=${q}&aqi=${aqi}&alerts=${alerts}`
     let promise = await fetch(query, { method: 'GET' })
       .catch((error) => console.error('weatherapi forecast error: ' + error))
     const wx = await promise.json()
