c26fdab77d06ac7e674b1de3c725cb1d9729f142
Collin D. Bevins <cbevins@montana.com>
2021-05-23 19:39:43 -0600

    Added esa and esaSource

--- src/routes/_stores.js
+++ src/routes/_stores.js
@@ -1,7 +1,14 @@
 import { writable } from 'svelte/store';
 
-// WeatherAPI Location object returned by surrent, forecast, and timezone services
+// WeatherAPI Location object returned by current, forecast, and timezone services
+// Set by 1location.svelte
 export const loc = writable(null)
+
+// Elevation, slope, aspect object
+// Set euqal to gel, mel, or uel by 2esa.svelte
+export const esa = writable(null)
+
+export const esaSource = writable('MapQuest')
 
 // Google elevation data store
 export const gel = writable(null)
